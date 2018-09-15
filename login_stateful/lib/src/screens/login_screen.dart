5c52642113b952a961007125ece8c9430999a66f
Dulio Denis <dulio.denis@yahoo.com>
2018-09-15 14:38:06 -0400

    Added Container Styling with a TextFormField in a Columnar Child Form

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-
   createState() {
     return LoginScreenState();
   }
-
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+         child: Column(
+           children: [
+             emailField(),
+            //  passwordField(),
+            //  submitButton(),
+           ],
+         )
+      ),
+    );
+  }
 
-  Widget build() {}
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@domain.com',
+      ),
+    );
+  }
 
+  Widget passwordField() {
+
+  }
+
+  Widget submitButton() {
+
+  }
 }
